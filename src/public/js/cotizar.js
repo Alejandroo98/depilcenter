f3b228c4bc41d7bedb0ebe99b0aea0f830481965
Alejandro <jeffersonalejandro27@gmail.com>
2021-02-01 23:24:22 -0500

    Ahora dependiendo del dominio cargara las diferentes partes ya sea  hombre mujer o combos . Podrias poner directamente en el link /hombre y cargaria la parte de toizacion para hombres

--- src/public/js/cotizar.js
+++ src/public/js/cotizar.js
@@ -24,21 +24,24 @@ socket.on('imprimirTotalCotizacioMujer', (valorTotal) => {
 
 /* ==================FIN IMPRIMIR PRECIO FINAL DE LA COTIZACION======================== */
 
+/* =======================PINTAR NAV========================== */
 let etiquetaPintar = document.querySelector('.cotizar');
 var URLactual = window.location;
 let URLpintar = URLactual.pathname;
-if (URLpintar === '/cotizar-combos') {
-  etiquetaPintar.classList.add('pintarNav');
-} else if (URLpintar === '/cotizar-combos/hombre') {
-  console.log('dentro');
-  document.getElementById('profile-tab').click();
-}
+/* =======================FIN PINTAR NAV========================== */
 
 // ================COTIZAR PRECIO=================
 let cajaMainCotizarMujer = document.querySelector('.cajaMainCotizarMujer');
 let clickCotizarHombre = document.querySelector('#profile-tab');
 let cajaCotizarCorporal = document.querySelector('.cajaCotizarCorporal');
 let cajaCotizarFacial = document.querySelector('.cajaCotizarFacial');
+let cajaCotizarCorporalHombre = document.querySelector(
+  '.cajaCotizarCorporalHombre'
+);
+let cajaCotizarFacialHombre = document.querySelector(
+  '.cajaCotizarFacialHombre'
+);
+
 // let preciosPagar = [0];
 // let sumador = 0;
 
@@ -116,14 +119,80 @@ class Cotizar {
       }
     });
   };
+
+  totalCotizarHombreCorporal = (id) => {
+    this.pintarDespintar(id);
+    socket.emit(
+      'sumarValoresHombreCorporal',
+      id,
+      function (dataCorporal, bool, numeroMayor) {
+        if (bool === true) {
+          document.querySelector(`.a${numeroMayor.id}`).innerHTML =
+            numeroMayor.precioIndividual;
+
+          for (let i = 0; i < dataCorporal.length; i++) {
+            document.querySelector(`.a${dataCorporal[i]._id}`).innerHTML =
+              dataCorporal[i].precioCombo;
+          }
+        } else if (bool === false) {
+          for (let i = 0; i < dataCorporal.length; i++) {
+            document.querySelector(`.a${dataCorporal[i]._id}`).innerHTML =
+              dataCorporal[i].precioIndividual;
+          }
+        }
+      }
+    );
+  };
+
+  totalCotizarHombreFacial = (id) => {
+    this.pintarDespintar(id);
+    socket.emit(
+      'sumarValoresHombreFacial',
+      id,
+      function (dataCorporal, bool, numeroMayor) {
+        if (bool === true) {
+          document.querySelector(`.a${numeroMayor.id}`).innerHTML =
+            numeroMayor.precioIndividual;
+
+          for (let i = 0; i < dataCorporal.length; i++) {
+            document.querySelector(`.a${dataCorporal[i]._id}`).innerHTML =
+              dataCorporal[i].precioCombo;
+          }
+        } else if (bool === false) {
+          for (let i = 0; i < dataCorporal.length; i++) {
+            document.querySelector(`.a${dataCorporal[i]._id}`).innerHTML =
+              dataCorporal[i].precioIndividual;
+          }
+        }
+      }
+    );
+  };
+
+  /*FIN FUNCIONES PARA COTIZACION DEL HOMBRE */
 }
 
 /* ====================EVENTOS DE COTIZACION============================== */
-clickCotizarHombre.addEventListener('click', (x) => {
+/* HOMBRE */
+let cargarDatosCotizarHombre = () => {
   let pintarCotizarHombre = new Cotizar();
   pintarCotizarHombre.cargarMostrarDatosCotizarHombre();
+};
+
+cajaCotizarCorporalHombre.addEventListener('click', (e) => {
+  if (e.target.id != '') {
+    let pintarCotizar = new Cotizar(e.target.id);
+    pintarCotizar.totalCotizarHombreCorporal(e.target.id);
+  }
 });
 
+cajaCotizarFacialHombre.addEventListener('click', (e) => {
+  if (e.target.id != '') {
+    let pintarCotizar = new Cotizar(e.target.id);
+    pintarCotizar.totalCotizarHombreFacial(e.target.id);
+  }
+});
+
+/* MUJER */
 cajaCotizarFacial.addEventListener('click', (e) => {
   if (e.target.id != '') {
     let pintarCotizar = new Cotizar(e.target.id); //El e.target.id de aqui no lo utilizamos pero por si necesitas el id de la etiqueta de etiqueta precionada la tendra de forma global
@@ -138,3 +207,28 @@ cajaCotizarCorporal.addEventListener('click', (e) => {
   }
 });
 /* ====================FIN EVENTOS DE COTIZACION============================== */
+
+/* =============ABIRI COTIZACION HOMBRE O MUJER ==================*/
+let idCotizar;
+let url = location.pathname;
+switch (url) {
+  case '/cotizar-combos/mujer':
+    document.getElementById('home-tab').classList.toggle('active');
+    document.getElementById('cotizarMujer').style.display = 'block';
+    document.getElementById('cotizarHombre').style.display = 'none';
+    document.getElementById('combosCotizar').style.display = 'none';
+    break;
+  case '/cotizar-combos/hombre':
+    document.getElementById('profile-tab').classList.toggle('active');
+    document.getElementById('cotizarHombre').style.display = 'block';
+    document.getElementById('cotizarMujer').style.display = 'none';
+    document.getElementById('combosCotizar').style.display = 'none';
+    cargarDatosCotizarHombre();
+    break;
+  case '/cotizar-combos/combos':
+    document.getElementById('contact-tab').classList.toggle('active');
+    document.getElementById('combosCotizar').style.display = 'block';
+    document.getElementById('cotizarMujer').style.display = 'none';
+    document.getElementById('cotizarHombre').style.display = 'none';
+}
+/* =============FIN ABIRI COTIZACION HOMBRE O MUJER =============*/
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,9 @@ const app = express();
 module.exports = function (url) {
   const routes = [
     '/',
-    '/cotizar-combos',
     '/cotizar-combos/hombre',
+    '/cotizar-combos/mujer',
+    '/cotizar-combos/combos',
     '/blog',
     '/ubicacion',
     '/servicios',
@@ -16,7 +17,11 @@ module.exports = function (url) {
   let comrpovarRuta = routes.includes(url);
   let urlCut = url.substr(1, url.length - 1);
 
-  if (url === '/cotizar-combos/hombre') {
+  if (
+    urlCut === 'cotizar-combos/hombre' ||
+    urlCut === 'cotizar-combos/mujer' ||
+    urlCut === 'cotizar-combos/combos'
+  ) {
     return router.get(url, (req, res, next) => {
       res.render('cotizar-combos');
     });
