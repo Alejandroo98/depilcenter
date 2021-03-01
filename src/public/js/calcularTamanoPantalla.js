ad1662aa5dbce26b9ce62bd2d984826b27cd5a22
Alejandro <jeffersonalejandro27@gmail.com>
2021-03-01 00:08:11 -0500

    Ultimos cambios al diseño de la pagia principal

--- src/public/js/calcularTamanoPantalla.js
+++ src/public/js/calcularTamanoPantalla.js
@@ -43,33 +43,24 @@ let activarClasss = new ajustarPantalla(tamVentana()[0], tamVentana()[1]);
 /* Lo que hace esto es crear una caja del tamaño de la caja de arriba luego  escrolea la pantalla y muestra sus elementos. Calculamos el tamaño del div  */
 class ajustarPantallaReserva {
   constructor(anchura, altura) {
-    this.div = document.querySelector('.cajaPrincipal');
     this.cajaReserva = document.querySelector('.reservarHBS');
-    this.cajaSegundaReservar = document.querySelector('.cajaSegundaReservar');
-    this.footer = document.querySelector('.boxFooter');
     this.click = document.getElementById('click');
     this.altura = altura;
     this.anchura = anchura;
-    this.ultimoScroll = 0;
-    this.bajarScroll();
-  }
-
-  bajarScroll() {
-    // this.click.click();
-    this.cajaReserva.scrollTop = 9999;
   }
 
   tamDiv() {
     let tamPantalla = `${this.altura}`;
-    // this.cajaReserva.style.height = `${this.altura}px`;
     return tamPantalla;
   }
 
   scroll() {
-    // this.cajaReserva.style.display = 'block';
+    this.cajaReserva.style.display = 'flex';
+    let scroll = this.tamDiv() / 2;
+    let scrollEnd = scroll / 2;
     setTimeout((x) => {
       window.scroll({
-        top: this.tamDiv(),
+        top: scroll + scrollEnd,
         behavior: 'smooth',
       });
     }, 20);
@@ -95,31 +86,8 @@ window.addEventListener('resize', () => {
   activarClass.tamDiv();
 });
 
-/* CONTROL DE SCROLL PARA PINTAR EL NAV */
-let lastScrollTop = 0;
-let pintarOpacity = 0;
-// window.addEventListener(
-//   'scroll',
-//   function () {
-//     let st = window.pageYOffset || document.documentElement.scrollTop;
-//     // let entero = st.toFixed();
-//     let divicion = st / 3;
-//     console.log(divicion);
-//     let navbar = document.querySelector('.navbar');
-//     if (st > divicion) {
-//       //baja
-//       console.log('Desaparecer');
-//     } else {
-//       //sube
-//       console.log('Mostrar');
-//     }
-//     lastScrollTop = st;
-//   },
-//   false
-// );
 window.onscroll = function () {
   let navbar = document.querySelector('.navbar');
-  console.log('Vertical: ' + window.scrollY);
   let vertical = window.scrollY;
   if (vertical > 380) {
     navbar.classList.add('navbarBackground');
