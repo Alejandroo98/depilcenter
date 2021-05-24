20e51015357faaad66b7266e421e8db762d95105
Alejandro <jeffersonaleandro27@gmail.com>
2021-05-23 21:47:15 -0500

    Diseñoe nuevo en form de reserva

--- src/public/js/calcularTamanoPantalla.js
+++ src/public/js/calcularTamanoPantalla.js
@@ -7,10 +7,7 @@ function tamVentana() {
     typeof document.documentElement.clientWidth != 'undefined' &&
     document.documentElement.clientWidth != 0
   ) {
-    tam = [
-      document.documentElement.clientWidth,
-      document.documentElement.clientHeight,
-    ];
+    tam = [document.documentElement.clientWidth, document.documentElement.clientHeight];
   } else {
     tam = [
       document.getElementsByTagName('body')[0].clientWidth,
@@ -19,8 +16,6 @@ function tamVentana() {
   }
   return tam;
 }
-
-
 
 class ajustarPantalla {
   constructor(anchura, altura) {
@@ -48,54 +43,44 @@ class ajustarPantallaReserva {
     this.altura = altura;
     this.anchura = anchura;
   }
-  
+
   tamDiv() {
     return {
-      anchura : this.anchura,
-      altura : this.altura
+      anchura: this.anchura,
+      altura: this.altura,
     };
   }
-  
-  ajustarSegundaPAntallaRserva(){
-    this.cajaReserva.style.height = this.tamDiv().altura + 'px';
+
+  ajustarSegundaPAntallaRserva() {
+    // this.cajaReserva.style.height = this.tamDiv().altura + 'px';
+    // this.cajaReserva.scrollIntoView({ block: 'center' });
   }
 
-    scroll() {
-      this.ajustarSegundaPAntallaRserva()
-      this.cajaReserva.style.display = 'flex';
-      let bajar = this.tamDiv().altura + (this.tamDiv().altura / 3)  ;
-      window.scroll({
-        top : bajar,
-        behavior: 'smooth'
-      })
+  scroll() {
+    this.cajaReserva.style.display = 'flex';
+    this.cajaReserva.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    // this.ajustarSegundaPAntallaRserva();
+    // let bajar = this.tamDiv().altura + this.tamDiv().altura / 3;
+    // window.scroll({
+    //   top: bajar,
+    //   behavior: 'smooth',
+    // });
   }
-  
-
 }
 
 // let data = false;
 
 window.addEventListener('resize', () => {
-  let activarClass = new ajustarPantallaReserva(
-    tamVentana()[0],
-    tamVentana()[1]
-  );
+  let activarClass = new ajustarPantallaReserva(tamVentana()[0], tamVentana()[1]);
   activarClass.ajustarSegundaPAntallaRserva();
 });
 
 document.querySelector('.agendarCita').addEventListener('click', () => {
-
-  let activarClassReserva = new ajustarPantallaReserva(
-    tamVentana()[0],
-    tamVentana()[1]
-  );
+  let activarClassReserva = new ajustarPantallaReserva(tamVentana()[0], tamVentana()[1]);
   activarClassReserva.scroll();
 });
 
-let activarClass = new ajustarPantallaReserva(
-  tamVentana()[0],
-  tamVentana()[1]
-);
+let activarClass = new ajustarPantallaReserva(tamVentana()[0], tamVentana()[1]);
 activarClass.ajustarSegundaPAntallaRserva();
 
 // window.addEventListener('resize', () => {
@@ -105,8 +90,6 @@ activarClass.ajustarSegundaPAntallaRserva();
 //   );
 //   activarClass.tamDiv();
 // });
-
-
 
 window.onscroll = function () {
   let navbar = document.querySelector('.navbar');
@@ -121,50 +104,46 @@ window.onscroll = function () {
 /*FIN CONTROL DE SCROLL PARA PINTAR EL NAV */
 
 /* ============= CAMBIAR FONDO DE CUMPLAÑEROS DEL MES ============== */
-let ajusarBackgroundCumpleanieros = ( ancho ) => {
-  if( ancho > 840 ){
-    imgCumpleanios.setAttribute("src" , "../img/background-cumpleanios_tre.svg")
-  }else if( ancho < 840 && ancho > 600){
-    imgCumpleanios.setAttribute("src" , "../img/background-cumpleanierosMedium.svg")
-  }else if( ancho < 600 ){
-    imgCumpleanios.setAttribute("src" , "../img/background-cumpleanierosSmoll.svg")
+let ajusarBackgroundCumpleanieros = (ancho) => {
+  if (ancho > 840) {
+    imgCumpleanios.setAttribute('src', '../img/background-cumpleanios_tre.svg');
+  } else if (ancho < 840 && ancho > 600) {
+    imgCumpleanios.setAttribute('src', '../img/background-cumpleanierosMedium.svg');
+  } else if (ancho < 600) {
+    imgCumpleanios.setAttribute('src', '../img/background-cumpleanierosSmoll.svg');
   }
-}
+};
 
-let imgCumpleanios = document.getElementById("imgCumpleanios");
+let imgCumpleanios = document.getElementById('imgCumpleanios');
 window.addEventListener('resize', () => {
-  ajusarBackgroundCumpleanieros( tamVentana()[0] )
+  ajusarBackgroundCumpleanieros(tamVentana()[0]);
 });
 
-ajusarBackgroundCumpleanieros( tamVentana()[0] )
+ajusarBackgroundCumpleanieros(tamVentana()[0]);
 
 /* =============*FIN CAMBIAR FONDO DE CUMPLAÑEROS DEL MES ============== */
 
 /* ========ROTAS IMAGENES DE EMOJIS ============= */
 
-let emojisImg = document.getElementById("emojisImg");
+let emojisImg = document.getElementById('emojisImg');
 
-class Emojis{
-
+class Emojis {
   tiempo = () => {
     setTimeout(() => {
-      this.rotarImg()
-    },8000)
-    
-  }
+      this.rotarImg();
+    }, 8000);
+  };
 
-   rotarImg = () => {
-     let num = Math.floor(Math.random() * 6)
-    emojisImg.setAttribute("src" , `../img/emojis/emojis-${num}.svg`)
+  rotarImg = () => {
+    let num = Math.floor(Math.random() * 6);
+    emojisImg.setAttribute('src', `../img/emojis/emojis-${num}.svg`);
 
-    this.tiempo()
+    this.tiempo();
+  };
 }
-}
 
+let rotar = new Emojis();
+rotar.tiempo();
 
-let rotar = new Emojis()
-rotar.tiempo()
-
-
-module.exports = tamVentana
+module.exports = tamVentana;
 /* ========ROTAS IMAGENES DE EMOJIS ============= */
